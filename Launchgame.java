15a886c5d55f5abe77ca8920ea99939b878b8b46
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-03-21 16:49:38 +0530

    Add files via upload

--- Launchgame.java
+++ Launchgame.java
@@ -1,76 +1,131 @@
-package Strings;
 import java.util.Scanner;
+
 class Guesser
 {
     int guessNum;
-    int guessingNumber()
+
+    public int guessNumber()
     {
         Scanner scan=new Scanner(System.in);
-        System.out.println("Guesser! Kindly guess the number");
-        guessNum= scan.nextInt();
+
+
+        System.out.println("Guesser kindly guess the number");
+        guessNum=scan.nextInt();
         return guessNum;
+
     }
+
 }
 
 class Player
 {
-    int guessNum;
-    int guessingNum()
+    int pguessNum;
+
+    public int guessNumber()
     {
         Scanner scan=new Scanner(System.in);
-        System.out.println("Player! Kindly guess the number");
-        guessNum= scan.nextInt();
-        return guessNum;
+        System.out.println("Player kindly guess the number");
+        pguessNum=scan.nextInt();
+        return pguessNum;
     }
 }
+
 class Umpire
 {
+
     int numFromGuesser;
     int numFromPlayer1;
     int numFromPlayer2;
     int numFromPlayer3;
-        void collectNumFromGuesser()
+
+
+    public void collectNumFromGuesser()
     {
-        Guesser n=new Guesser();
-        numFromGuesser= g.guessingNumber();
+        Guesser g=new Guesser();
+        numFromGuesser=g.guessNumber();
+
     }
-        void collectNumFromPlayers()
+
+
+    public void collectNumFromPlayer()
     {
         Player p1=new Player();
         Player p2=new Player();
         Player p3=new Player();
-        numFromPlayer1=p1.guessingNumber();
-        numFromPlayer2=p2.guessingNumber();
-        numFromPlayer3=p3.guessingNumber();
+
+
+        numFromPlayer1=p1.guessNumber();
+        numFromPlayer2=p2.guessNumber();
+        numFromPlayer3=p3.guessNumber();
     }
-        void compare()
+
+    void compare()
     {
+
         if(numFromGuesser==numFromPlayer1)
         {
-            System.out.println("Player1 Won the game");
+            if(numFromGuesser==numFromPlayer2 && numFromGuesser==numFromPlayer3)
+            {
+                System.out.println("Game tied all three players guessed correctly");
+            }
+            else if(numFromGuesser==numFromPlayer2)
+            {
+                System.out.println("Player 1 and Player2 won the game");
+            }
+            else if(numFromGuesser==numFromPlayer3)
+            {
+                System.out.println("Player 1 and Player3 won ");
+            }
+            else
+            {
+
+                System.out.println("Player 1 won the game");
+            }
         }
-        else if (numFromGuesser==numFromPlayer2)
+
+
+        else if(numFromGuesser==numFromPlayer2)
         {
-           System.out.println("Player2 won the game");
+
+            if(numFromGuesser==numFromPlayer3)
+            {
+                System.out.println("Player 2 and Player3 won the game");
+            }
+            else
+            {
+                System.out.println("Player 2 won the game");
+            }
         }
-        else if (numFromGuesser==numFromPlayer3)
+
+
+
+        else if(numFromGuesser==numFromPlayer3)
         {
-            System.out.println("Player3 Won the game");
+            System.out.println("Player 3 won the game");
         }
         else
         {
-            System.out.println("Sorry! Lose the game");
+            System.out.println("Game lost! try again");
         }
-
     }
+
 }
+
+
+
+
+
 public class Launchgame {
-    public static void main(String []args){
-            System.out.println("Game Started");
-            Umpire u=new Umpire();
-            u.collectNumFromGuesser();
-            u.collectNumFromPlayers();
-            u.compare();
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+
+        Umpire u=new Umpire();
+        u.collectNumFromGuesser();
+        u.collectNumFromPlayer();
+        u.compare();
 
     }
+
 }
