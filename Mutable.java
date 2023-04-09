bbe93c786a144ae1ca1faeb425bd3cc5ab5e28fe
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-04-09 12:02:23 +0530

    Add files via upload

---
+++ Mutable.java
@@ -0,0 +1,14 @@
+public class Mutable {
+    public static void main(String []args)
+    {
+     //   StringBuffer sb=new StringBuffer("Virat");
+       // System.out.println(sb);
+        //sb.append(" Kohli");
+        //System.out.println(sb);
+
+        StringBuilder sb=new StringBuilder("Virat");
+        System.out.println(sb);
+        sb.append(" Kohli");
+        System.out.println(sb);
+    }
+}
