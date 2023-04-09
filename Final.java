044ace8b7f4adaa2d5aeb17472606927bdbb82bd
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-04-09 12:23:09 +0530

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,27 @@
+public class Final {
+    public static void main(String []args)
+    {
+       // int a=10;
+       // a=20;
+        // System.out.println(a);
+
+        // final int a=10;
+         //a=20;
+         //System.out.println(a);
+
+        //final int a=10;
+        //System.out.println(a);
+
+       //Final StringBuffer sb=new StringBuffer("Virat");
+        //sb.append(" Kohli");
+        //System.out.println(sb);
+        //sb=new StringBuffer("Sachin");
+        //System.out.println(sb);
+
+        StringBuffer sb=new StringBuffer("Virat");
+        sb.append(" Kohli");
+        System.out.println(sb);
+        sb=new StringBuffer("Sachin");
+        System.out.println(sb);
+    }
+}
