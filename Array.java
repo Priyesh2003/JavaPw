5973cd21bf4862c8358b6fbfd65cff5c424ff3ed
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-04-10 23:38:56 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String []args)
+    {
+        int nums[][]={{2,7},{9,3},{5,6}};
+        for (int i=0;i<=2;i++)
+        {
+            for (int j=0;j<=1;j++)
+            {
+                System.out.print(nums[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
