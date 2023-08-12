28001b9e04a8b33cada6b689e7ee29aaacc94c32
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-08-12 23:09:35 +0530

    Add files via upload

---
+++ Inheritance2.java
@@ -0,0 +1,17 @@
+package Inheritance;
+class  Animal
+{
+    void sleep()
+    {System.out.println("Animal need sleep");}
+}
+class Tiger extends Animal
+{
+
+}
+public class Inheritance2  {
+    public static void main(String[] args)
+    {
+        Tiger t=new Tiger();
+        t.sleep();
+    }
+}
