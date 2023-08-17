222294723dc0f61486172861974a5076a6b397fe
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-08-17 20:59:10 +0530

    Add files via upload

--- LaunchPoly1.java
+++ LaunchPoly1.java
@@ -11,18 +11,72 @@ class AeroPlane2
         System.out.println("AeroPlane is flying");
     }
 }
-class CArgoPlane extends AeroPlane2
+class CargoPlane2 extends AeroPlane2
 {
     @Override
     public void takeoff() {
-        System.out.println("CarrgoPlane requires longer runway");
+        System.out.println("CargoPlane requires longer runway");
     }
 
     @Override
     public void fly() {
-        System.out.println("Carg");
+        System.out.println("Cargoplane flies at lower height");
+    }
+}
+class PassengerPlane2 extends AeroPlane2
+{
+    @Override
+    public void takeoff() {
+        System.out.println("PassengerPlane requires medium size runway");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("PassengerPlane flies at medium height");
+    }
+}
+class FighterPlane2 extends AeroPlane2
+{
+    @Override
+    public void takeoff() {
+        System.out.println("Fighterplane requires smaller runway");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Fighterplane flies at higher height");
+    }
+}
+class Airport
+{
+    public void poly(AeroPlane2 ref)
+    {
+        ref.fly();
+        ref.takeoff();
+
+        System.out.println(".......");
     }
 }
 
 public class LaunchPoly1 {
+    public static void main(String[] args)
+    {
+        CargoPlane2 cp=new CargoPlane2();
+        PassengerPlane2 pp=new PassengerPlane2();
+        FighterPlane2 fp=new FighterPlane2();
+
+        Airport a=new Airport();
+        a.poly(cp);
+        a.poly(pp);
+        a.poly(fp);
+
+       //off( cp.fly();
+       // cp.takeoff();
+
+        //pp.fly();
+        //pp.takeoff();
+
+        //fp.fly();
+        //fp.takeoff();
+    }
 }
