ac87c8831e6cdcfcea79a90bbf10e95ce378b414
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-08-21 22:50:15 +0530

    Add files via upload

---
+++ AnnoyDemo.java
@@ -0,0 +1,12 @@
+@FunctionalInterface
+interface Car
+{
+    void drive(int avg, int topSpeed);
+}
+public class AnnoyDemo {
+    public static void main(String[] args)
+    {
+        Car obj=((avg, topSpeed) -> System.out.println("Running with avg" + avg));
+        obj.drive(16, 140);
+    }
+}
