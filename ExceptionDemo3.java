867c335ea1d5265a3d5636a6fe315f7e3eca3e45
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-08-26 23:18:21 +0530

    Add files via upload

---
+++ ExceptionDemo3.java
@@ -0,0 +1,22 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionDemo3 {
+
+        public static void main(String[] args)
+        {
+            int num=0;
+            Scanner sc=new Scanner(System.in);
+
+            try{
+                num=sc.nextInt();
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.println("Please enter number");
+            }
+            sc.close();
+
+            System.out.println(num);
+        }
+    }
