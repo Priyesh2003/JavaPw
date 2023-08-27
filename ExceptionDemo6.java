15a68000abcd96aea0f91714d411dffa2c8e5996
Priyesh2003 <114237012+Priyesh2003@users.noreply.github.com>
2023-08-27 23:29:44 +0530

    Add files via upload

---
+++ ExceptionDemo6.java
@@ -0,0 +1,31 @@
+class Demo
+{
+    public void a()
+    {
+        System.out.println(" in a");
+        int value=9;
+
+        b();
+    }
+    public void b()
+    {
+        int num1=9;
+        int num2=0;
+        try
+        {
+            int result=num1/num2;
+            System.out.println(result);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error" + e.getMessage());
+        }
+    }
+}
+public class ExceptionDemo6 {
+    public static void main(String[] args)
+    {
+        Demo obj=new Demo();
+        obj.a();
+    }
+}
